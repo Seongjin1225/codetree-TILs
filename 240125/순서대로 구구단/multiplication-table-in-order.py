07af1c7987c2ec73f910891f6d1c04273a2967f4
Seongjin1225 <114036940+Seongjin1225@users.noreply.github.com>
2024-01-25 16:22:27 +0900

    Title: 순서대로 구구단, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240125/순서대로 구구단/multiplication-table-in-order.py
+++ 240125/순서대로 구구단/multiplication-table-in-order.py
@@ -8,5 +8,5 @@ if a > b:
 else:
     for i in range(1,10):
         for j in range(a,b+1):
-            print(f"{j} * {i} = {i * j}", end="\t")
+            print(f"{j} * {i} = {i * j}", end="  ")
         print()
