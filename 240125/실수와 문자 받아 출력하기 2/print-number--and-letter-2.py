3e7308ad17b9bcff5cb5bb54bb4d8cd522292781
Seongjin1225 <114036940+Seongjin1225@users.noreply.github.com>
2024-01-25 16:07:36 +0900

    Title: 실수와 문자 받아 출력하기 2, Time: 200ms, Memory: 24MB, Status: Passed - CodeTree

--- 240125/실수와 문자 받아 출력하기 2/print-number--and-letter-2.py
+++ 240125/실수와 문자 받아 출력하기 2/print-number--and-letter-2.py
@@ -2,6 +2,9 @@ s = input()
 x = float(input())
 y = float(input())
 
-print(round(y,2))
-print(round(x,2))
+y_rounded = round(y,2)
+x_rounded = round(x,2)
+
+print(f'{y_rounded:.2f}')
+print(f'{x_rounded:.2f}')
 print(s)
