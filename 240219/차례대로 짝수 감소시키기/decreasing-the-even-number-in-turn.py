fd0842f33cb77f8aa8d13a7153bc038c3101c2e7
Seongjin1225 <114036940+Seongjin1225@users.noreply.github.com>
2024-02-19 15:20:11 +0900

    Title: 차례대로 짝수 감소시키기, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240219/차례대로 짝수 감소시키기/decreasing-the-even-number-in-turn.py
@@ -0,0 +1,15 @@
+n = int(input())
+num = 1000
+i = 1
+lst = []
+
+while True:
+    num -= i*2
+    lst.append(i*2)
+    if num <= n:
+        break
+    i += 1
+
+sum_val = sum(lst)
+print(len(lst), end=' ')
+print(sum_val)
