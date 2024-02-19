40e5913bfa769fc563a1a5689d46ede0744ff74c
Seongjin1225 <114036940+Seongjin1225@users.noreply.github.com>
2024-02-19 14:14:15 +0900

    Title: 알파벳에 따른 평가 2, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240219/알파벳에 따른 평가 2/evaluation-by-alphabet-2.py
@@ -0,0 +1,14 @@
+word = input()
+
+if word == 'S':
+    print('Superior')
+elif word == 'F':
+    print('Fantastic')
+elif word == 'G':
+    print('Good')
+elif word == 'U':
+    print('Usually')
+elif word == 'E':
+    print('Effort')
+else:
+    print('Failure')
