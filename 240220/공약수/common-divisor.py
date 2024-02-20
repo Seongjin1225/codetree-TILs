b292af8493ca6b4a73421967bf2a41972394929c
Seongjin1225 <114036940+Seongjin1225@users.noreply.github.com>
2024-02-20 14:04:49 +0900

    Title: 공약수, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240220/공약수/common-divisor.py
+++ 240220/공약수/common-divisor.py
@@ -3,12 +3,12 @@ num = tuple(map(int,input().split()))
 
 if n == 2:
     a,b = num
-    for i in range(1,a+1):
+    for i in range(1,min(a,b)+1):
         if(a%i) == 0 and (b%i)==0:
             print(i)
 
 else:
     a,b,c = num
-    for i in range(1,a+1):
-        if a%i == 0 & b%i ==0 & c%i ==0:
+    for i in range(1,min(a,b,c)+1):
+        if (a%i == 0) & (b%i ==0) & (c%i ==0):
             print(i)
