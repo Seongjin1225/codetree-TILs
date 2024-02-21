34d87cc72c07311091fe05550dce0fd4c98dadf1
Seongjin1225 <114036940+Seongjin1225@users.noreply.github.com>
2024-02-21 15:52:55 +0900

    Title: 문자열 찾기, Time: 178ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/문자열 찾기/search-string.py
+++ 240221/문자열 찾기/search-string.py
@@ -1,8 +1,10 @@
 sentence = input()
 cnt,cnt_2 = 0,0
-if 'KOI' in sentence:
-    cnt += 1
-if 'IOI' in sentence:
-    cnt_2 +=1
+
+for i in range(len(sentence)-2):
+    if sentence[i:i+3] == 'KOI':
+        cnt += 1
+    if sentence[i:i+3] == 'IOI':
+        cnt_2 +=1
 
 print(cnt, cnt_2)
