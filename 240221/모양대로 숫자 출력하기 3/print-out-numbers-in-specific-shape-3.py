a23c8715055b46788e5230b5b1690fdd8cb27618
Seongjin1225 <114036940+Seongjin1225@users.noreply.github.com>
2024-02-21 13:25:26 +0900

    Title: 모양대로 숫자 출력하기 3, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/모양대로 숫자 출력하기 3/print-out-numbers-in-specific-shape-3.py
+++ 240221/모양대로 숫자 출력하기 3/print-out-numbers-in-specific-shape-3.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(n):
-    print(' ',end='')
-    for j in range(1, n-i+1):
-        print(j,end=' ')
+    print(' ' * (2 * i), end='')  
+    for j in range(1, n - i + 1):
+        print(j, end=' ') 
     print()
