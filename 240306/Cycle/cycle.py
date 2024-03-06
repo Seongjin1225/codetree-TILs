577b81c297fc1595b9433e1ae99580f9966b9201
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-03-06 23:10:26 +0900

    Title: Cycle, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/Cycle/cycle.py
+++ 240306/Cycle/cycle.py
@@ -1,16 +1,13 @@
 n,p = tuple(map(int,input().split()))
 
-cnt = 0
-lst = [n]
+lst = []
 
 start = n
 while True:
     val = (start*n)%p
     if val not in lst:
         lst.append(val)
-        cnt += 1
     else:
+        print(len(lst)-lst.index(val))
         break
     start = val
-    
-print(cnt)
