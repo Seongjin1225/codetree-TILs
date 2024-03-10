eacb9526cd73e09ae33d4a0e0ec3f12554b0feeb
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-03-10 16:04:35 +0900

    Title: 영문 소문자의 개수, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/영문 소문자의 개수/number-of-lowercase-letters.py
+++ 240310/영문 소문자의 개수/number-of-lowercase-letters.py
@@ -4,14 +4,16 @@ sen = input().split()
 frequency = {}
 
 for elem in sen:
-    if elem.isupper():
+    if elem.isupper() or elem.isdigit():
         idx = sen.index(elem)
-        sen_2 = sen[:idx]
-        for elem in sen_2:
-            if elem in frequency:
-                frequency[elem] += 1
-            else:
-                frequency[elem] = 1
+        sen = sen[:idx]
+        break
+
+for el in sen:
+    if el in frequency:
+        frequency[el] += 1
+    else:
+        frequency[el] = 1
 
 for key, value in sorted(frequency.items()):
     print(f"{key} : {value}")
