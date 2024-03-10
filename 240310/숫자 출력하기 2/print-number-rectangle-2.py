24f4fbbf1bbe9007344f47632f121c18aea7247e
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-03-10 14:48:11 +0900

    Title: 숫자 출력하기 2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240310/숫자 출력하기 2/print-number-rectangle-2.py
@@ -0,0 +1,19 @@
+n,m = tuple(map(int,input().split()))
+
+arr = [[0 for _ in range(m)] for _ in range(n)]
+
+num = 0
+for i in range(n):
+    if i%2 == 0:
+        for j in range(m):
+            num += 1
+            arr[i][j] += num
+    else:
+        for j in range(m-1,-1,-1):
+            num +=1 
+            arr[i][j] += num
+
+for elem in arr:
+    for e in elem:
+        print(e,end= ' ')
+    print()
