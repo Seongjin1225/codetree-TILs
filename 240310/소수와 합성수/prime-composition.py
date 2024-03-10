985dd173427794798082942f8def242dd4ec745a
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-03-10 14:12:50 +0900

    Title: 소수와 합성수, Time: 172ms, Memory: 25MB, Status: Passed - CodeTree

--- 240310/소수와 합성수/prime-composition.py
+++ 240310/소수와 합성수/prime-composition.py
@@ -6,11 +6,13 @@ cnt = 0
 if n == 1:
     print('one')
 else:
+    is_prime = True
     num = math.sqrt(n)
     for i in range(2,int(num)+1):
         if n%i == 0:
-            print('composition')
+            is_prime = False
             break
-        else:
-            print('prime')
-            break
+    if is_prime:
+        print('prime')
+    else:
+        print('composition')
