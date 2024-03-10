8f2fc562497124c0af3049bcecefb2f415e4c634
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-03-10 14:22:22 +0900

    Title: -999, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/-999/-999.py
+++ 240310/-999/-999.py
@@ -1,6 +1,6 @@
 arr = list(map(int,input().split()))
 
-for i in range(len(arr)):
-    if arr[i] == -999:
-        arr = arr[:i]
+if -999 in arr:
+    i = arr.index(-999)
+    arr = arr[:i]
 print(max(arr), min(arr))
