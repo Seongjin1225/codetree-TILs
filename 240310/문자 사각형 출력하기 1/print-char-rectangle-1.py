86ae2ee01a5d8c578f67450cbec1466462bb43d3
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-03-10 15:22:01 +0900

    Title: 문자 사각형 출력하기 1, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240310/문자 사각형 출력하기 1/print-char-rectangle-1.py
+++ 240310/문자 사각형 출력하기 1/print-char-rectangle-1.py
@@ -2,11 +2,14 @@ n = int(input())
 arr = [['' for _ in range(n)] for _ in range(n)]
 
 num = 65 # ASCII 코드 시작 
+         # 90 = Z이므로 90까지만
 
 for i in range(n-1,-1,-1):
     for j in range(n-1,-1,-1):
         arr[j][i] += chr(num)
         num += 1
+        if num > 90:
+            num = 65
 
 for elem in arr:
     for e in elem:
