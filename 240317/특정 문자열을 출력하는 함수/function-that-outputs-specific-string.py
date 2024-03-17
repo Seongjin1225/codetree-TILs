a32803c99c8b2fd842a716ed63b95c967d9092ff
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-03-17 15:10:08 +0900

    Title: 특정 문자열을 출력하는 함수, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240317/특정 문자열을 출력하는 함수/function-that-outputs-specific-string.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+def show(num):
+    for _ in range(num):
+        print('Hello')
+        print('#@#@#@#@#@')
+        print('CodeTree')
+        print('@#@#@#@#@#')
+        print('Students!')
+        print()
+
+show(n)
