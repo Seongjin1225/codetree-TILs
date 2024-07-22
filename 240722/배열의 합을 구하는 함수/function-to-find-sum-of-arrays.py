c2e62cad0f52eebf3fbad5fda28c0d8a1778e96e
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-07-22 16:18:23 +0900

    Title: 배열의 합을 구하는 함수, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/배열의 합을 구하는 함수/function-to-find-sum-of-arrays.py
+++ 240722/배열의 합을 구하는 함수/function-to-find-sum-of-arrays.py
@@ -1,12 +1,24 @@
 n = int(input())
 arr = []
-for in range(n):
+for _ in range(n):
     arr.append(list(map(int,input().split())))
-print(sum(arr[1]))
-# def extend(arr):
-#     n = len(arr)
+def extend(arr):
+    n = len(arr)
 
-#     new_arr = [[0]*(n+1) for _ in range(n+1)]
+    new_arr = [[0]*(n+1) for _ in range(n+1)]
 
-#     for i in range(n):
-#         sum_1 = sum(arr[i])
+    for i in range(n):
+        sum_1 = sum(arr[i])
+        new_arr[i][n] = sum_1   
+        for j in range(n):
+            new_arr[i][j] = arr[i][j]
+            new_arr[n][j] += arr[i][j]
+    new_arr[n][n] = sum(new_arr[n][:-1])
+    return new_arr
+
+ans = extend(arr)
+
+for elem in ans:
+    for e in elem:
+        print(e,end=' ')
+    print()
