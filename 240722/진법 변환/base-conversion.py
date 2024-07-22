c256b549734bdc48ae7f6ddaeb048edac466a0d5
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-07-22 16:52:10 +0900

    Title: 진법 변환, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/진법 변환/base-conversion.py
+++ 240722/진법 변환/base-conversion.py
@@ -2,10 +2,13 @@ n,m = tuple(input().split())
 ans = 0
 
 if int(m) < 10:
-    for i in range(len(str(n))):
-        ans += (int(m)**i)*int(str(n)[i])
+    for i in range(len(str(n))-1,-1,-1):
+        ans += (int(m)**(len(n)-i-1))*int(str(n)[i])
     print(ans)
 else:
-    for i in range(len(str(n))):
-        ans += (int(m)**i)*(ord(n[i])-55)
+    for i in range(len(str(n))-1,-1,-1):
+        if n[i].isdigit():
+            ans += (int(m)**(len(n)-i-1))*int(str(n)[i])
+        else:
+             ans += (int(m)**(len(n)-i-1))*(ord(n[i])-55)
     print(ans)
