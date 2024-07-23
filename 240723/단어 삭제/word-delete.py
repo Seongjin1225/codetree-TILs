bf8fff161e3c3831136ac0a06a254a42e218c69b
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-07-23 23:43:37 +0900

    Title: 단어 삭제, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/단어 삭제/word-delete.py
+++ 240723/단어 삭제/word-delete.py
@@ -2,7 +2,7 @@ word1 = input()
 word2 = input()
 n = len(word2)
 
-for i in range(len(word1)-1):
-    if word1[i:i+n] == word2:
-        word1 = word1.replace(word2,'')
+while word2 in word1:
+    word1 = word1.replace(word2, '')
+
 print(word1)
