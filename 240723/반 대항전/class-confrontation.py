2444b9ac373de222888bd6c199ebd375bddd1ccd
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-07-23 23:28:35 +0900

    Title: 반 대항전, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/반 대항전/class-confrontation.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+arr = []
+ans = []
+alp = 65
+for _ in range(4):
+    arr.append(list(input().split()))
+
+val = 0
+for i in range(len(arr)):
+    for j in range(1,n+1):
+        val += int(arr[i][j])
+    ans.append([chr(alp),val])
+    alp+=1
+    val = 0
+
+for elem in ans:
+    print(elem[0],"-",elem[1])
+
+print('Class {} is winner!'.format(max(ans)[0]))
