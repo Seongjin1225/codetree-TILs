17edd177f08e52081e12b529a17bbaab8a04e26b
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-07-26 16:38:51 +0900

    Title: 개발자의 컴퓨터, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240726/개발자의 컴퓨터/developer's-computers.py
+++ 240726/개발자의 컴퓨터/developer's-computers.py
@@ -1,14 +1,29 @@
+# n = int(input())
+# arr = []
+# for _ in range(n):
+#     arr.append(list(map(int,input().split())))
+
+# max_val = max(max(arr))
+# new_arr = [0]*(max_val+1)
+
+# for i in arr:
+#     start,end,plus = i
+#     for j in range(start,end+1):
+#         new_arr[j] += plus
+
+# print(max(new_arr))
 n = int(input())
 arr = []
 for _ in range(n):
-    arr.append(list(map(int,input().split())))
+    arr.append(list(map(int, input().split())))
 
-max_val = max(max(arr))
-new_arr = [0]*(max_val+1)
+# 각 리스트의 두 번째 요소들 중 최대값을 찾아서 max_val을 설정
+max_val = max([end for _, end, _ in arr])
+new_arr = [0] * (max_val + 1)
 
 for i in arr:
-    start,end,plus = i
-    for j in range(start,end+1):
+    start, end, plus = i
+    for j in range(start, end + 1):
         new_arr[j] += plus
 
 print(max(new_arr))
