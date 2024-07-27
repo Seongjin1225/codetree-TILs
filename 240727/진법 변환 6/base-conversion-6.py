3dcec6b9608577c6725336df0dbb65c57fe40c82
SeongJin Im <114036940+Seongjin1225@users.noreply.github.com>
2024-07-27 16:35:15 +0900

    Title: 진법 변환 6, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/진법 변환 6/base-conversion-6.py
+++ 240727/진법 변환 6/base-conversion-6.py
@@ -1,25 +1,71 @@
-a,n,b = input().split()
-tmp = 0
-leng = len(n)
-ans = []
+# a,n,b = input().split()
+# tmp = 0
+# leng = len(n)
+# ans = []
+
+# # n을 10진수로 변환
+# if n.isalpha():
+#     num = ord(n)-87
+#     for i in range(leng-1,-1,-1):
+#         tmp += int(num[i])*(2**(leng-i-1))
+# else:
+#     for i in range(leng-z1,-1,-1):
+#         tmp += int(n[i])*(2**(leng-i-1))
+
+# # 10진수를 b진수로 변환
+# while True:
+#     if tmp < int(b):
+#         ans.append(tmp)
+#         break
+#     val = tmp%int(b) 
+#     if val < 10:
+#         ans.append(tmp%int(b))
+#     else:
+#         ans.append(chr(val+87))
+#     tmp //=int(b)
+
+# ans.reverse()
+# for elem in ans:
+#     print(elem,end='')
+
+def convert_to_decimal(n, base):
+    """ Convert a number n from base `base` to decimal (base 10). """
+    decimal_value = 0
+    length = len(n)
+    for i in range(length):
+        digit = n[length - 1 - i]
+        if '0' <= digit <= '9':
+            decimal_value += (ord(digit) - ord('0')) * (base ** i)
+        else:
+            decimal_value += (ord(digit) - ord('a') + 10) * (base ** i)
+    return decimal_value
+
+def convert_from_decimal(decimal, base):
+    """ Convert a decimal (base 10) number to `base` base. """
+    if decimal == 0:
+        return "0"
+    
+    result = []
+    while decimal > 0:
+        remainder = decimal % base
+        if remainder < 10:
+            result.append(chr(remainder + ord('0')))
+        else:
+            result.append(chr(remainder - 10 + ord('a')))
+        decimal //= base
+    
+    return ''.join(result[::-1])
+
+# 입력 받기
+a, n, b = input().split()
+a = int(a)
+b = int(b)
 
 # n을 10진수로 변환
-if n.isalpha():
-    num = ord(n)-87
-    for i in range(leng-1,-1,-1):
-        tmp += int(num[i])*(2**(leng-i-1))
-else:
-    for i in range(leng-1,-1,-1):
-        tmp += int(n[i])*(2**(leng-i-1))
+decimal_value = convert_to_decimal(n, a)
 
 # 10진수를 b진수로 변환
-while True:
-    if tmp < int(b):
-        ans.append(tmp)
-        break
-    ans.append(tmp%int(b))
-    tmp //=int(b)
+result = convert_from_decimal(decimal_value, b)
 
-ans.reverse()
-for elem in ans:
-    print(elem,end='')
+# 결과 출력
+print(result)
